7daa6a9456b75678b1a5503617afa18ede67928a
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-02-15 13:35:33 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { getToken } from '../store/action';
 
 class Login extends React.Component {
   constructor() {
@@ -11,7 +13,6 @@ class Login extends React.Component {
     };
     this.handleOnInputChange = this.handleOnInputChange.bind(this);
     this.loginButtonDisabled = this.loginButtonDisabled.bind(this);
-    this.initGame = this.initGame.bind(this);
   }
 
   handleOnInputChange({ target }) {
@@ -31,20 +32,13 @@ class Login extends React.Component {
     }
   }
 
-  async initGame() {
-    const response = await fetch('https://opentdb.com/api_token.php?command=request');
-    const token = await response.json();
-    const { history } = this.props;
-    localStorage.setItem('token', JSON.stringify(token));
-    history.push('/game');
-  }
-
   render() {
     const {
       loginEmail,
       loginName,
       isLoginButtonDisabled,
     } = this.state;
+    const { history, handleTokenClick } = this.props;
     return (
       <div>
         <form>
@@ -75,12 +69,15 @@ class Login extends React.Component {
             data-testid="btn-play"
             name="isSaveButtonDisabled"
             disabled={ isLoginButtonDisabled }
-            onClick={ this.initGame }
+            onClick={ () => {
+              handleTokenClick();
+              history.push('/game');
+            } }
           >
             Play
           </button>
           <button
-            type="submit"
+            type="button"
             data-testid="btn-settings"
           >
             Settings
@@ -92,9 +89,16 @@ class Login extends React.Component {
 }
 
 Login.propTypes = {
+  handleTokenClick: PropTypes.func.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
 };
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  handleTokenClick: () => {
+    dispatch(getToken());
+  },
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,6 +1,26 @@
-const action = (value) => ({
+export const SUCESS_RESPONSE = 'SUCESS_RESPONSE';
+
+export const action = (value) => ({
   type: 'CLICK_UPDATE_VALUE',
   newValue: value,
 });
 
-export default action;
+function sucessResponse(token) {
+  return {
+    type: SUCESS_RESPONSE,
+    token,
+  };
+}
+
+export function getToken() {
+  return async (dispeatch) => {
+    try {
+      const response = await fetch('https://opentdb.com/api_token.php?command=request');
+      const token = await response.json();
+      localStorage.setItem('token', JSON.stringify(token));
+      dispeatch(sucessResponse(token.token));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
