73efb6668ae6cdbd75ea60bbbe1a797a0e1d6074
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-02-15 14:56:48 -0300

    req4

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,16 +1,9 @@
 export const LOGIN = 'LOGIN';
 
-const action = (value) => ({
-  type: 'CLICK_UPDATE_VALUE',
-  newValue: value,
-});
-
-export function login({ name, email }) {
+export function login(name, email) {
   return {
     type: LOGIN,
     name,
     email,
   };
 }
-
-export default action;
