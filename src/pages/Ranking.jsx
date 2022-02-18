5819517cf82ac71e16969ce28b2bdca39d815ef8
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-02-18 14:20:16 -0300

    requisito 17

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -14,6 +14,7 @@ class Ranking extends React.Component {
         <button
           type="button"
           onClick={ this.initialPage }
+          data-testid="btn-go-home"
         >
           inicio
         </button>
