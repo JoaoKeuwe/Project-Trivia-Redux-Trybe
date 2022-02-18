09a838e5e5d370c5dc7382280f87cec9ec6c8827
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-02-18 14:15:46 -0300

    req 17

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  initialPage = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Tela de Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Tela de Ranking</h1>
+        <button
+          type="button"
+          onClick={ this.initialPage }
+        >
+          inicio
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
